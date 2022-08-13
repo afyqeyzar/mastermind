67bf91cb55f2bc23d725e9b2346953a85558824a
afyqeyzar <norafyqe@gmail.com>
2022-08-13 16:07:22 +0100

    loop still dont work

--- computer.rb
+++ computer.rb
@@ -33,35 +33,11 @@ class Game
         @player_guess = PlayerGuess.new().get_guess
     end
 
-    # def compare
-    #     @computer_array.each_with_index do |comp_number, comp_index|
-    #         @player_guess.each_with_index do |player_str, player_index|
-    #             if player_str == comp_number.to_s and comp_index == player_index
-    #                 print player_str.green
-    #             elsif player_str == comp_number.to_s and comp_index != player_index
-    #                 print player_str.yellow
-    #             else
-    #                 print player_str
-    #             end
-    #         end
-    #     end
-    # end
-
     def compare
-        # @computer_array.each_with_index do |comp_number, comp_index|
-        #     if comp_number.to_s == @player_guess[comp_index]
-        #         print player_guess[comp_index].green
-        #     elsif @player_guess.include?(comp_number.to_s) and comp_number.to_s != @player_guess[comp_index]
-        #         print player_guess[comp_index].to_s.yellow
-        #     else
-        #         print player_guess[comp_index]
-        #     end
-        # end
-
         @player_guess.each_with_index do |player_guess, player_index|
             if player_guess == @computer_array[player_index].to_s
                 print player_guess.green
-            elsif @computer_array.include?(player_guess.to_i) and player_guess.to_i != @computer_array[player_index]
+            elsif @computer_array.include?(player_guess.to_i) #&& player_guess == @computer_array[player_index].to_s
                 print player_guess.yellow
             else
                 print player_guess
