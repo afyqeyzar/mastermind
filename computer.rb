9e44ded0131cb3c88c02cf4ace100d140d54fdcd
afyqeyzar <norafyqe@gmail.com>
2022-08-13 15:53:44 +0100

    loop works better

--- computer.rb
+++ computer.rb
@@ -48,13 +48,23 @@ class Game
     # end
 
     def compare
-        @computer_array.each_with_index do |comp_number, comp_index|
-            if comp_number.to_s == @player_guess[comp_index]
-                print player_guess[comp_index].green
-            elsif @player_guess.include?(comp_number.to_s)
-                print player_guess(comp_number.to_s.yellow)
+        # @computer_array.each_with_index do |comp_number, comp_index|
+        #     if comp_number.to_s == @player_guess[comp_index]
+        #         print player_guess[comp_index].green
+        #     elsif @player_guess.include?(comp_number.to_s) and comp_number.to_s != @player_guess[comp_index]
+        #         print player_guess[comp_index].to_s.yellow
+        #     else
+        #         print player_guess[comp_index]
+        #     end
+        # end
+
+        @player_guess.each_with_index do |player_guess, player_index|
+            if player_guess == @computer_array[player_index].to_s
+                print player_guess.green
+            elsif @computer_array.include?(player_guess.to_i) and player_guess.to_i != @computer_array[player_index]
+                print player_guess.yellow
             else
-                print player_guess[comp_index]
+                print player_guess
             end
         end
     end
